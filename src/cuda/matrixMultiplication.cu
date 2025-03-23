9e27bcb3522011062cffd80844a8834af764747f
Ryan Fudge <ryandfudge@gmail.com>
2025-03-23 00:46:40 -0300

    Add matrix multiplication

---
+++ src/cuda/matrixMultiplication.cu
@@ -0,0 +1,17 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <device_launch_parameters.h>
+
+__global__ void matrix_multiplication_kernel(const float* A, const float* B, float* C, int M, int N, int K) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y; 
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < M && col < K) {
+        float sum = 0.0f;
+        for (int i = 0; i < N; i++) {
+            sum += A[row * N + i] * B[i * K + col];
+        }
+        C[row * K + col] = sum;
+    }
+
+}
